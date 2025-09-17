e396eba3f5ed393f87b90c4fc67363944199d35c
JuanEstebanAparicio <apariciojuanesteban@gmail.com>
2025-09-17 04:52:43 -0500

    commit con supabaseUrl y key a un paso para empezar la configuracion de supabase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -11,7 +11,11 @@ export const environment = {
     storageBucket: "wallpy-62beb.firebasestorage.app",
     messagingSenderId: "880991682714",
     appId: "1:880991682714:android:772bf4c2a85927986c9f23"
-  }
+  },
+
+  supabaseUrl: 'https://uaxgsngscwzgavefjypb.supabase.co',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVheGdzbmdzY3d6Z2F2ZWZqeXBiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTgwOTQ2NjksImV4cCI6MjA3MzY3MDY2OX0.gV7xYJ_arYVZdFtMC9FxCZpdeBWBI2-bR5g14ouujNk'
+
 };
 
 /*
