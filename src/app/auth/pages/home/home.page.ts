4be917beeb537d77e35dea4777baabd7a0401517
JuanEstebanAparicio <apariciojuanesteban@gmail.com>
2025-09-17 04:04:45 -0500

    implementacion de home con interfaz + proteccion

--- src/app/auth/pages/home/home.page.ts
+++ src/app/auth/pages/home/home.page.ts
@@ -1,13 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { Auth, onAuthStateChanged, signOut } from '@angular/fire/auth';
+import { Router } from '@angular/router';
+import { UiService } from '../../../shared/services/ui.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  standalone: false,
+  standalone: false
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  private auth = inject(Auth);
+  private router = inject(Router);
+  private ui = inject(UiService);
+  
+  displayName: string | null = null;
+  email: string | null = null;
 
   constructor() {}
+  ngOnInit(): void {
+        onAuthStateChanged(this.auth, (user) => {
+      if (user) {
+        this.displayName = user.displayName;
+        this.email = user.email;
+      }
+    });
 
+  }
+
+    async logout() {
+    await signOut(this.auth);
+    this.ui.showSuccess('Sesión cerrada');
+    this.router.navigateByUrl('/auth/login', { replaceUrl: true });
+  }
 }
+
