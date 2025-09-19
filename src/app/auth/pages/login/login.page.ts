5c205e154558aaa4f1af9dc0b84ff6bbf4da0cfd
JuanEstebanAparicio <apariciojuanesteban@gmail.com>
2025-09-19 16:28:41 -0500

    rediseño de metodo login para que cunsulte a firebase

--- src/app/auth/pages/login/login.page.ts
+++ src/app/auth/pages/login/login.page.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
 import { UiService } from 'src/app/shared/services/ui.service';
 import { SupabaseService } from '../../services/supabase.service';
+import { Auth } from '@angular/fire/auth'
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
@@ -13,12 +14,18 @@ export class LoginPage {
   email = '';
   password = '';
 
-  constructor(private authService: AuthService, private router: Router, private ui: UiService, private supabaseService: SupabaseService) {}
+  constructor(private authService: AuthService, private router: Router, private ui: UiService, private supabaseService: SupabaseService, private auth: Auth) {}
 
 async onLogin() {
   try {
     // 1️⃣ Iniciar sesión en Firebase
     await this.authService.login(this.email, this.password);
+
+    // 2️⃣ Obtener token de Firebase y configurarlo en Supabase
+    const firebaseUser = this.auth.currentUser;
+    if (!firebaseUser) throw new Error('No hay sesión en Firebase');
+    const token = await firebaseUser.getIdToken();
+    this.supabaseService.setAuthToken(token);
 
     this.ui.showSuccess('¡Bienvenido de nuevo!');
     this.router.navigateByUrl('/home', { replaceUrl: true });
