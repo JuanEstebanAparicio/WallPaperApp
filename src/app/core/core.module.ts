c05fa54e699138987269de005e15e2ffce04f4d8
JuanEstebanAparicio <apariciojuanesteban@gmail.com>
2025-09-19 22:14:41 -0500

    commit con libreria

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,6 +3,9 @@ import { AngularFireModule } from '@angular/fire/compat';
 import { AngularFireAuthModule } from '@angular/fire/compat/auth';
 import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 import { environment } from 'src/environments/environment';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
 
 @NgModule({
   imports: [
